1a39cc1926fbbf5e222f665cab5939bb28db0199
eyereece <eyereece@windowslive.com>
2024-10-23 10:07:02 -0700

    connected db and tested displaying them in templates

--- nlp_dashboard/nlp_dashboard/nlp_app/views.py
+++ nlp_dashboard/nlp_dashboard/nlp_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Articles
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    articles = Articles.objects.all()[:10]
+    return render(request, 'home.html', {
+        'articles': articles
+    })
--- nlp_dashboard/nlp_dashboard/nlp_dashboard/settings.py
+++ nlp_dashboard/nlp_dashboard/nlp_dashboard/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,9 +81,13 @@ WSGI_APPLICATION = 'nlp_dashboard.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
 
