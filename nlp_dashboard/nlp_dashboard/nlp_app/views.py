1eb13b2a7c73b0e59a6e82b1496c65f46ee40355
eyereece <eyereece@windowslive.com>
2024-10-23 09:45:46 -0700

    connected js and css

--- nlp_dashboard/nlp_dashboard/nlp_app/views.py
+++ nlp_dashboard/nlp_dashboard/nlp_app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, world! Welcome to my first Django app.")
+    return render(request, 'home.html')
